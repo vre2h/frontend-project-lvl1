9605dbf6dbe25909ac2bae5a5430cecacaf0ae9c
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-03-03 20:24:45 +0400

    feat(*): add welcoming message

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import readlineSync from "readline-sync";
+
+const app = () => {
+  console.log("Welcome to the Brain Games! \n");
+
+  const answer = readlineSync.question("May I have your name? ");
+
+  console.log(`Hello, ${answer}!`);
+};
+
+export default app;
