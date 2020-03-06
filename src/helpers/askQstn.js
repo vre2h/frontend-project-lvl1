6aa2df241bb943b69e4cbc45114b98fd8bcc0e56
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-03-06 12:28:29 +0400

    feat(helpers): add jsdoc comments

--- src/helpers/askQstn.js
+++ src/helpers/askQstn.js
@@ -1,3 +1,8 @@
 import readlineSync from 'readline-sync';
 
+/**
+ * Simple function which is asking user given question and returns response
+ * @param {string} qstn - qstn that readlineSync going to ask
+ * @returns {string} answer that user typed
+ */
 export default (qstn) => readlineSync.question(qstn);
--- src/helpers/isEven.js
+++ src/helpers/isEven.js
@@ -1,1 +1,5 @@
+/**
+ * @param {number} num - number that need to be checked
+ * @returns {boolean} - is given number even
+ */
 export default (num) => num % 2 === 0;
