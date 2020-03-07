ac0393bf6dcfec7be94e95c8352ed61f27caaad0
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-03-07 22:37:24 +0400

    feat(randomizer): add helper for working with random numbers

---
+++ src/helpers/randomizer.js
@@ -0,0 +1,15 @@
+/**
+ * returns random elem between min and max
+ * @param {number} min
+ * @param {number} max
+ */
+const getRandomSegment = (min = 0, max = 10) =>
+  Math.floor(Math.random() * (max - min) + min);
+
+/**
+ * returns elem before max
+ * @param {number} max
+ */
+const getRandomBefore = (max) => Math.floor(Math.random() * max);
+
+export { getRandomSegment, getRandomBefore };
